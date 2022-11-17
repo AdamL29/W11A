dcf7d72933a45542945dbf213b4e858acc83eb40
Adam <adamlederer@live.ca>
2022-11-16 22:49:06 -0700

    attempted page change. Ran into permanent page changes

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-// function makeCookies(event){
-//     Cookies.set(`selection`, `Color`);
-// }
-// document.getElementById(`color`).addEventListener(`click`, makeCookies);
-
-// function makeCookies(event){
-//     Cookies.set(`selection`, `Pokemon`);
-// }
-// document.getElementById(`pokemon`).addEventListener(`click`, makeCookies);
-
-// function makeCookies(event){
-//     Cookies.set(`selection`, `Drinks`);
-// }
-// document.getElementById(`drinks`).addEventListener(`click`, makeCookies);
-
-function SelectionMade(){
-    let currentUser = Cookies.get(`selection`);
-    if (currentUser == `Color`){
-        Cookies.set(`selection`, `Pokemon`);
-    } else if (currentUser == `Color`){
-        Cookies.set(`selection`, `Drinks`)
-    } else if (currentUser == `Drinks`){
-        Cookies.set(`selection`, `Pokemon`)
-    }
+function selectColor(){
+    Cookies.set(`selection`, `Colors`)
 }
+document.getElementById(`color`).addEventListener(`click`, selectColor)
 
-document.getElementById(`selections`).addEventListener(`click`, SelectionMade)
+function selectPokemon(){
+    Cookies.set(`selection`, `Pokemon`)
+}
+document.getElementById(`pokemon`).addEventListener(`click`, selectPokemon)
+
+function selectDrinks(){
+    Cookies.set(`selection`, `Drinks`)
+}
+document.getElementById(`drinks`).addEventListener(`click`, selectDrinks)
+
 
 // location.href = `selection.html`
