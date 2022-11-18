d9cbdca0dcdcd6cb4c26974af654e1e15be29a97
Adam <adamlederer@live.ca>
2022-11-17 17:51:14 -0700

    made coding line adujustment. Made 2nd js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 function selectColor(){
-    Cookies.set(`selection`, `Colors`)
+    Cookies.set(`selection1`, `Colors`)
 }
 document.getElementById(`color`).addEventListener(`click`, selectColor)
 
+
 function selectPokemon(){
-    Cookies.set(`selection`, `Pokemon`)
+    Cookies.set(`selection2`, `Pokemon`)
 }
 document.getElementById(`pokemon`).addEventListener(`click`, selectPokemon)
 
+
 function selectDrinks(){
-    Cookies.set(`selection`, `Drinks`)
+    Cookies.set(`selection3`, `Drinks`)
 }
 document.getElementById(`drinks`).addEventListener(`click`, selectDrinks)
 
